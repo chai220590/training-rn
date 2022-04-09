55be5d3d8180555961c228bb664e649542c83167
chai220590 <chai.220590@gmail.com>
2022-04-09 09:11:32 +0700

    done

--- src/screens/user_manage/index.js
+++ src/screens/user_manage/index.js
@@ -1,23 +1,7 @@
 import {View, Text} from 'react-native';
 import React, {useEffect} from 'react';
-import axios from 'axios';
 
 const UserManageScreen = () => {
-  useEffect(() => {
-    getUserList();
-  }, []);
-
-  const getUserList = () => {
-    axios.get({
-      url: 'https://thaoquan.herokuapp.com/api/user',
-    });
-    // .then(result => {
-    //   console.log(result);
-    // })
-    // .catch(error => {
-    //   console.log('fetch data fail');
-    // });
-  };
   return (
     <View>
       <Text>UserManageScreen</Text>
