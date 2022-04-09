ed05fa3414daf2a787cb01047b3d4c4b935c96d1
chai220590 <chai.220590@gmail.com>
2022-04-09 09:10:07 +0700

    05 fail

--- src/screens/user_manage/index.js
+++ src/screens/user_manage/index.js
@@ -1,7 +1,23 @@
 import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useEffect} from 'react';
+import axios from 'axios';
 
 const UserManageScreen = () => {
+  useEffect(() => {
+    getUserList();
+  }, []);
+
+  const getUserList = () => {
+    axios.get({
+      url: 'https://thaoquan.herokuapp.com/api/user',
+    });
+    // .then(result => {
+    //   console.log(result);
+    // })
+    // .catch(error => {
+    //   console.log('fetch data fail');
+    // });
+  };
   return (
     <View>
       <Text>UserManageScreen</Text>
